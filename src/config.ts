9398eb7aa12708e5d3f75485e384577ec2cc6835
LorienYang <yangluoying19277904322@outlook.com>
2026-04-02 23:46:18 +0800

    初次提交

--- src/config.ts
+++ src/config.ts
@@ -26,7 +26,7 @@ const SITE_TIMEZONE = 8; //设置你的网站时区 from -12 to 12 default in UT
 export const siteConfig: SiteConfig = {
 	title: "Sakura Reverie",
 	subtitle: "樱之境",
-	siteURL: "https://www.sakurin.cn/", // 请替换为你的站点URL，以斜杠结尾
+	siteURL: "https://blog.sakurin.cn/", // 请替换为你的站点URL，以斜杠结尾
 	siteStartDate: "2026-04-02", // 站点开始运行日期，用于站点统计组件计算运行天数
 
 	timeZone: SITE_TIMEZONE,
